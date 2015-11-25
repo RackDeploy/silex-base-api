df3d224af9085563691914263403cae3c6bf2519
Thomas Cooper <tom.cooper@rackdeploy.com>
2015-11-25 15:57:57 -0500

    updated namespace and class name for yaml loader

--- src/controllers.php
+++ src/controllers.php
@@ -6,8 +6,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+// Overrides to process swagger yaml as routing config
 use Symfony\Component\Config\FileLocator;
-use Overrides\CustomYamlFileLoader;
+use Overrides\SwaggerYamlFileLoader;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
@@ -22,8 +23,9 @@ $app->get('0.0/test/', function () use ($app) {
     return $app['twig']->render('api.json', array('test' => $test));
 });
 
+// Build routes from swagger.yaml
 $app['routes'] = $app->share($app->extend('routes', function ($routes, $app) {
-    $loader = new CustomYamlFileLoader(new FileLocator(__DIR__.'/../config'));
+    $loader = new SwaggerYamlFileLoader(new FileLocator(__DIR__.'/../config'));
     $collection = $loader->load('swagger.yaml');
     $routes->addCollection($collection);
 
