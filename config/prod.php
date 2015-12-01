8f98f70a39b025f7c7dd581ff90d86ba7b16aee7
Thomas Cooper <tom.cooper@rackdeploy.com>
2015-12-01 11:35:49 -0500

    moved yaml expose to config

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,9 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+// Expose Swagger.yaml to swagger-ui
+$app->get('api-docs/omni-api.yaml', function () use ($app) {
+    $file = file_get_contents('../config/swagger.yaml', FILE_USE_INCLUDE_PATH);
+    return $file;
+});
